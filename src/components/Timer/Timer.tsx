4acdccd5e1395df335a7d75c722ebf2dd4142a30
solusmax <solusmaxim@gmail.com>
2023-12-07 11:35:51 +0300

    Add styles

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -22,9 +22,30 @@ export default function Timer(): JSX.Element {
 
   return (
     <>
-      <div>
-        <a href="https://www.rockstargames.com/VI">GTA VI</a> is at least
-        {daysUntilGTA6Release} days away from release.
+      <div
+        className="flex min-h-full flex-col items-center justify-center bg-gradient-to-tr
+          from-blue-700 from-5% via-pink-500 via-60% to-orange-300 p-4 pb-8
+          text-xl font-bold leading-none text-white sm:text-2xl sm:leading-none"
+      >
+        <div className="mb-5 transition-opacity hover:opacity-90 active:opacity-75">
+          <a
+            href="https://www.rockstargames.com/VI"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="sr-only">GTA VI</span>
+            <img
+              className="w-[240px] sm:w-[360px]"
+              src="img/logo-gta6.png"
+              alt="GTA 6 logo."
+            />
+          </a>
+        </div>
+        <div>is at least</div>
+        <div className="mb-3 text-[7rem] font-bold sm:text-[9rem]">
+          {daysUntilGTA6Release}
+        </div>
+        <div>days away from release</div>
       </div>
     </>
   );
